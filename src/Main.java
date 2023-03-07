bae4ab38743f21392e00b3cbc7e84ddab1badb73
Minojka <stankovic_mina@yahoo.com>
2023-03-07 20:05:54 +0100

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void  main(String[] args){
         MathProcessor mathProcessor = new MathProcessor();
-        mathProcessor.process(i:2, e: 3);
+        mathProcessor.process(2,  3);
 
 
 
